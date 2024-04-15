17cf1eee5546b564ea9bce660ac41e6d5a92b7ed
lantrungseo <lantrungseo@gmail.com>
2024-04-15 17:38:00 +0700

    feat: add csv-parse

--- scripts/ai-poc/seed-course.ts
+++ scripts/ai-poc/seed-course.ts
@@ -2,7 +2,14 @@ import dotenv from 'dotenv'
 dotenv.config();
 import { openAI } from '@/lib/openai'
 import { elasticsearchClient } from '@/lib/elasticsearch';
+import { parse } from 'csv-parse/sync'
+import fs from 'fs'
 
-elasticsearchClient.ping()
-  .then(() => console.log('Elasticsearch client is connected'))
-  .catch(err => console.error('Elasticsearch client connection error', err))
+// elasticsearchClient.ping()
+//   .then(() => console.log('Elasticsearch client is connected'))
+//   .catch(err => console.error('Elasticsearch client connection error', err))
+
+parse(fs.readFileSync(`__data__/Coursera.csv`), {
+  skipEmptyLines: true,
+  columns: true,
+})
